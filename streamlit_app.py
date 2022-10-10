257a2b448c04124e0d055f54488d5ef9a9521111
Geoff Swinburn <115485539+GeoffSnowflake7@users.noreply.github.com>
2022-10-10 18:02:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit 
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom''s New Healthy Diner')
 
 streamlit.header("Breakfast favourites")
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smootie')
 streamlit.text('🐔 Hard Boilder Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')                
-               
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')               
                  
