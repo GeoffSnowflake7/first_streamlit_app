485a417d4cb205d1ae03956cc2417a2bca81e1c5
Geoff Swinburn <115485539+GeoffSnowflake7@users.noreply.github.com>
2022-10-10 17:53:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit 
 
 streamlit.title('My Parents new Healthy Diner')
+
+streamlit.header("Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smootie')
+streamlit.text('Hard Boilder Free-Range Egg')
+               
+                 
